a0e265e640fe5dfbf104986254702a9701ff3605
DELL <DELL@10.10.2.13>
2022-05-06 22:40:13 +0530

    1

---
+++ SeleniumProj/src/ScannerA2.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ScannerA2 
+{
+	public int sum(int a,int b)
+	{
+		int c;
+		c=a+b;
+		System.out.println("sum result:"+c);
+		return c;
+	}
+	
+	public int sub(int x,int y)
+	{
+		int z;
+		z=x-y;
+		System.out.println("sub result:"+z);
+		return z;
+	}
+	public int multi(int a1,int b1)
+	{
+		int c1;
+		c1=a1*b1;
+		System.out.println("multi result:"+c1);
+		return c1;
+	}
+	public void divide(int p,int q)
+	{
+		int r;
+		r=p/q;
+		System.out.println("divide result:"+r);
+	}
+	
+	public static void main(String[] args) 
+	{
+		ScannerA2 s1=new ScannerA2();
+		System.out.println("Please enter values");
+		Scanner s=new Scanner(System.in);
+		int x1=s.nextInt();
+		int x2=s.nextInt();
+		int x3=s.nextInt();
+		int x4=s.nextInt();
+		int x5=s.nextInt();
+		int x6=s.nextInt();
+		
+		int sub=s1.sub(x1, x2);
+		int sum=s1.sum(sub, x3);
+		int multi=s1.multi(sum, x4);
+		int sub1=s1.sub(multi, x5);
+		s1.divide(sub1, x6);
+		
+		
+	}
+}
